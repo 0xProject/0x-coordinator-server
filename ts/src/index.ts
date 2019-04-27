ec37d434d5c57ef8e92787339d8f82027dbf5946
xianny <xianny@gmail.com>
2019-04-26 20:40:52 -0700

    add more exports so users can change db configs

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,1 +1,5 @@
 export { getAppAsync } from './app';
+export { OrderEntity } from './entities/order_entity';
+export { TakerAssetFillAmountEntity } from './entities/taker_asset_fill_amount_entity';
+export { TransactionEntity } from './entities/transaction_entity';
+export { defaultOrmConfig } from './default_ormconfig';
